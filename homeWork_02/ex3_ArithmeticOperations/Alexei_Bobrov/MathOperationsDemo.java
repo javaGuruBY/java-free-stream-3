73e8e1e36dbe65a7fe0e2c8790488464829508cf
Alexei Bobrov <61884077+alex11087@users.noreply.github.com>
2020-03-17 13:23:40 +0300

    Create MathOperationsDemo.java

---
+++ homeWork_02/ex3_ArithmeticOperations/Alexei_Bobrov/MathOperationsDemo.java
@@ -0,0 +1,15 @@
+public class MathOperationsDemo {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 8;
+
+        System.out.println("Число num1 = " + num1);
+        System.out.println("Число num2 = " + num2);
+
+        System.out.println("Сложение. num1 + num1 = " + (num1+num2));
+        System.out.println("Вычитание. num1 - num2 = " + (num1-num2));
+        System.out.println("Деление. num1 / num2 = " + (num1/num2));
+        System.out.println("Умножение. num1 * num2 = " + (num1*num2));
+    }
+
+}
