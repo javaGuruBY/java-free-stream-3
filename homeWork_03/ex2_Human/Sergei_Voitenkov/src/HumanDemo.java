83ea0cb446bf50000957e5a2a4103938a9481786
Sergei Voitenkov <mistertopdeveloper@gmail.com>
2020-03-19 13:15:05 +0300

    Please, review hw3_task2

---
+++ homeWork_03/ex2_Human/Sergei_Voitenkov/src/HumanDemo.java
@@ -0,0 +1,8 @@
+public class HumanDemo {
+    public static void main(String[] args) {
+        Human person1 = new Human("Ваня", 25);
+        Human person2 = new Human("Катя", 20);
+        person1.greet();
+        person2.greet();
+    }
+}
