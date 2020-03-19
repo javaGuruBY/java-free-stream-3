28dd59881c120ba2476ad97994cba7537c26eb01
Artem Nefedov <nefedov1011@gmail.com>
2020-03-19 23:34:38 +0300

    change the name of variable

--- homeWork_02/ex3_ArithmeticOperations/Artem_Nefedov_Hw2_ex3/Artem Nefedov/src/MathOperationsDemo.java
+++ homeWork_02/ex3_ArithmeticOperations/Artem_Nefedov_Hw2_ex3/Artem Nefedov/src/MathOperationsDemo.java
@@ -1,46 +1,42 @@
 public class MathOperationsDemo {
     public static void main(String[] args) {
         System.out.println("Welcome to the 'Math Operations' ");
-        int a = 82;
-        int b = 10;
-        System.out.println("1) Суммирование '+': " );
-        System.out.print("a =");
-        System.out.print(a);
+        int footballViewers = 82;
+        int basketballViewers = 10;
+        System.out.println("1) Суммирование '+': ");
+        System.out.print("football Viewers =");
+        System.out.print(footballViewers);
         System.out.print(", ");
-        System.out.print("b =");
-        System.out.print(b);
-        int summ = a + b;
+        System.out.print("basketball Viewers =");
+        System.out.print(basketballViewers);
+        int summ = footballViewers + basketballViewers;
         System.out.println();
-        System.out.print("a + b =");
+        System.out.print("Total viewers = football Viewers + basketball Viewers =");
         System.out.println(summ);
-     double c = 15.8;
-     double d = 75.5;
-        System.out.println("2) Вычитание '-': " );
-        System.out.print("с =" + c + ", d = " + d);
-        double difference = d - c;
+        double priceForApple = 15.8;
+        double priceForOrange = 75.5;
+        System.out.println("2) Вычитание '-': ");
+        System.out.print("price For Apple =" + priceForApple + ", price For Orange = " + priceForOrange);
+        double difference = priceForOrange - priceForApple;
         System.out.println();
-        System.out.print("d - c =");
+        System.out.print("Price for orange - price for apple =");
         System.out.print(difference);
         System.out.println();
-        System.out.println("3) Деление '/': " );
-       long e = 24;
-       long f = 4;
-        System.out.println("e =" + e + ", f =" + f);
-        long division = e / f;
-        System.out.print("e / f =");
+        System.out.println("3) Деление '/': ");
+        long schoolStudents = 24;
+        long universityStudents = 4;
+        System.out.println("school Students =" + schoolStudents + ", University Students =" + universityStudents);
+        long division = schoolStudents / universityStudents;
+        System.out.print("School Students/ University Students =");
         System.out.print(division);
         System.out.println();
-        System.out.println("4) Умножение '*': " );
-        byte w = 11 ;
-        byte q  = 9;
-        System.out.println("w =" + w + ", q =" + q);
-        long multiplication  = w * q;
-        System.out.print("w * q =");
+        System.out.println("4) Умножение '*': ");
+        byte num1 =11;
+        byte num2 = 9;
+        System.out.println("Number 1 =" + num1 + ", Number 2 =" + num2);
+        long multiplication = num1 * num2;
+        System.out.print("Number 1 * Number 2 =");
         System.out.print(multiplication);
-
-
-
-
 
 
     }
