e0fb88e9357e772081e0cdf1e2668ab9a62b9526
Pasha <pasazhe@yandex.by>
2020-03-19 23:01:20 +0300

    Hw2Task3

---
+++ homeWork_02/ex3_ArithmeticOperations/Pasha_Vinnikov/src/MathOperationsDemo1.java
@@ -0,0 +1,43 @@
+public class MathOperationsDemo1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the 'Math Operations!'");
+        int k = 100;
+        int i = 51;
+        System.out.println ("1)"+" addition '+'" + ":");
+        System.out.println ("k = 100" + ", " + "i = 51" + ";");
+        System.out.print ("k + i");
+        System.out.print (" =");
+        System.out.print (" 100 + 51");
+        int result = k+i;
+        System.out.print(" = " + result);
+        System.out.println();
+        System.out.println();
+
+        System.out.println("2) Subtraction '-' :");
+        System.out.println("x = 3, y = 2;");
+        int x = 3;
+        int y = 2;
+        int subtraction = x - y;
+        System.out.println("x - y = " + x + " - " + y + " = " + subtraction);
+        System.out.println();
+
+        System.out.println("3)" + " Multiplication" + " '*':");
+        float a = 3.8f;
+        float b = 4.11f;
+        System.out.println("a = 3.8, b = 4.11;");
+        System.out.print ("a * b = 3.8 * 4.11 = ");
+        float multiplication = a * b;
+        System.out.println (multiplication);
+        System.out.println();
+
+        System.out.println ("4) Division '/' :");
+        int n = 20;
+        int m = 6;
+        float division = 20/6f ;
+        System.out.println ("n = 20, m = 6;");
+        System.out.println ("n / m = 20 / 6 = " + division);
+
+
+
+    }
+}
