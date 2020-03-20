0e57099c02353c77965cc462b1c34fbfb7901c81
Dimitar84 <46647887+Dimitar84@users.noreply.github.com>
2020-03-20 14:19:18 +0300

    third task

---
+++ homeWork_02/ex3_ArithmeticOperations/Dmitriy_Sokirko/source/MathOperationsDemo.java
@@ -0,0 +1,11 @@
+public class MathOperationsDemo {
+
+    public static void main(String[] args) {
+        double firstNum  = 4;
+        double secondNum  = 3;
+        System.out.println("a + b = " + (firstNum + secondNum));
+        System.out.println("a - b = " + (firstNum - secondNum));
+        System.out.println("a * b = " + (firstNum * secondNum));
+        System.out.println("a / b = " + (firstNum / secondNum));
+    }
+}
