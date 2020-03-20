2ea0ca01a2f487ddbc646048457f934fe280bed4
Rabbi Kazhimov <54858286+Rabbiba-2@users.noreply.github.com>
2020-03-20 15:06:40 +0300

    RabbiHw2Task3

---
+++ homeWork_02/ex3_ArithmeticOperations/Rabbi/RabbiHw1_task3/src/by/art/hw2Task3/MathOperationsDemo.java
@@ -0,0 +1,13 @@
+package by.art.hw2Task3;
+
+public class MathOperationsDemo {
+
+    public static void main(String[] args) {
+        double firstNum  = 27;
+        double secondNum  = 9;
+        System.out.println("Сложение. a + b = " + (firstNum + secondNum));
+        System.out.println("Вычитание. a - b = " + (firstNum - secondNum));
+        System.out.println("Умножение. a * b = " + (firstNum * secondNum));
+        System.out.println("Деление. a / b = " + (firstNum / secondNum));
+    }
+}
