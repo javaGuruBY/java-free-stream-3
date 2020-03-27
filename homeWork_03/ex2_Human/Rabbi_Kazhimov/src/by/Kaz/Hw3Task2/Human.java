a9553aa201644e126ef7d7fa6bdeb493170a7099
Rabbi Kazhimov <54858286+Rabbiba-2@users.noreply.github.com>
2020-03-27 05:49:27 +0300

    Rabbi Hw3 Task2

---
+++ homeWork_03/ex2_Human/Rabbi_Kazhimov/src/by/Kaz/Hw3Task2/Human.java
@@ -0,0 +1,14 @@
+package by.Kaz.Hw3Task2;
+
+public class Human {
+    private String name;
+    private int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    void greet() {
+        System.out.println("Hi! My name is " + name + ". I'm " + age + " years old.");
+    }
+}
---
+++ homeWork_03/ex2_Human/Rabbi_Kazhimov/src/by/Kaz/Hw3Task2/HumanDemo.java
@@ -0,0 +1,10 @@
+package by.Kaz.Hw3Task2;
+
+public class HumanDemo {
+    public static void main(String[] args) {
+        Human human1 = new Human("Адам", 21);
+        Human human2 = new Human("Ева", 20);
+        human1.greet();
+        human2.greet();
+    }
+}
